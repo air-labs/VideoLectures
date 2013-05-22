038691dc660c1ff0ec497cbe7ff8a9f0cf832ef9
okulovsky <yuri.okulovsky@gmail.com>
2013-05-22 21:14:27 +0600

    Несколько экспериментов с ffmpeg

---
+++ experiments/commands.bat
@@ -0,0 +1,8 @@
+PATH C:\ffmpeg\bin\
+del *.mp4
+
+ffmpeg -loop 1 -f image2 -i 01.png -c:v libx264 -t 1 01.mp4
+ffmpeg -loop 1 -f image2 -i 02.png -c:v libx264 -t 1 02.mp4
+ffmpeg -loop 1 -f image2 -i 03.png -c:v libx264 -t 1 03.mp4
+ffmpeg -f concat -i ConcatFilesList.txt -c copy 123.mp4
+ffmpeg -i 123.mp4 -ss 0.5 -t 2 1223.mp4
