8896fe524e48db6f1d4f3e2492a95535d4a3b69c
okulovsky <yuri.okulovsky@gmail.com>
2013-11-08 13:24:07 +0500

    Заработало. Надо дополнительно украсить

--- AIML/CSharp/Perceptron/Perceptron.cs
+++ AIML/CSharp/Perceptron/Perceptron.cs
@@ -13,13 +13,13 @@ namespace Perceptron
 {
     static class Program
     {
-        static Func<bool, bool, bool> Function = (x, y) => x && y;
+        static Func<bool, bool, bool> Function = (x, y) => x ^  y;
         static List<int> LearningOrder = new List<int>
         {
              1,  1,
-            -1,  1,
-             1, -1,
-            -1, -1,
+             0,  1,
+             1,  0,
+             0,  0,
         };
 
 
@@ -46,50 +46,50 @@ namespace Perceptron
 
         static void DrawSpace(object sender, PaintEventArgs e)
         {
-            if (weights == null) return;
-            S = Math.Min(form.ClientSize.Width, form.ClientSize.Height)/4f;
-            K = -weights[1] / weights[2];
-            C = -weights[0] / weights[2];
-            
-            var g = e.Graphics;
-            g.Clear(Color.White);
+            try
+            {
+                if (weights == null) return;
+                S = Math.Min(form.ClientSize.Width, form.ClientSize.Height) / 4f;
+                K = -weights[1] / weights[2];
+                C = -weights[0] / weights[2];
 
-            var arrowPen = new Pen(Color.Black, 2) { EndCap = LineCap.ArrowAnchor };
-            g.DrawLine(arrowPen, Plot(-2, 0), Plot(2, 0));
-            g.DrawLine(arrowPen, Plot(0, -2), Plot(0, 2));
-            var linePen = new Pen(Color.Black, 2) { DashStyle = DashStyle.Dash };
-            g.DrawLine(linePen, Plot(-2), Plot(2));
+                var g = e.Graphics;
+                g.Clear(Color.White);
 
-            for (int x=-1;x<=1;x+=2)
-                for (int y = -1; y <= 1; y += 2)
-                {
-                    var result = weights[1] * x + weights[2] * y + weights[0];
-                    var brush = result < 0 ? Brushes.White : Brushes.Black;
-                    var center = Plot(x, y);
-                    var ellipse=new RectangleF(center.X-10,center.Y-10,20,20);
-                    g.FillEllipse(brush, ellipse);
-                    g.DrawEllipse(Pens.Black, ellipse);
-                }
+                var arrowPen = new Pen(Color.Black, 2) { EndCap = LineCap.ArrowAnchor };
+                g.DrawLine(arrowPen, Plot(-2, 0), Plot(2, 0));
+                g.DrawLine(arrowPen, Plot(0, -2), Plot(0, 2));
+                var linePen = new Pen(Color.Black, 2) { DashStyle = DashStyle.Dash };
+                g.DrawLine(linePen, Plot(-2), Plot(2));
+
+                for (int x = 0; x <= 1; x++)
+                    for (int y = 0; y <= 1; y++)
+                    {
+                        var result = weights[1] * x + weights[2] * y + weights[0];
+                        var brush = result < 0 ? Brushes.White : Brushes.Black;
+                        var center = Plot(x, y);
+                        var ellipse = new RectangleF(center.X - 10, center.Y - 10, 20, 20);
+                        g.FillEllipse(brush, ellipse);
+                        g.DrawEllipse(Pens.Black, ellipse);
+                    }
+            }
+            catch { }
         }
 
-        static double D(bool b)
-        {
-            return b?1:-1;
-        }
 
         static void NextSample(object sender, EventArgs e)
         {
             var x=LearningOrder[LearningOrderPointer];
-            var y=LearningOrder[LearningOrderPointer];
+            var y=LearningOrder[LearningOrderPointer+1];
             LearningOrderPointer+=2;
             if (LearningOrderPointer >= LearningOrder.Count) LearningOrderPointer = 0;
             
             var input = new double[] { x,y };
-            var output = new double[] { Function(x > 0, y > 0) ? 1 : -1 };
+            var output = new double[] { Function(x > 0, y > 0) ? 1 : 0 };
+           
             learning.Run(input, output);
-
-
-            weights = new double[3];
+            
+            
             weights[0] = ((ActivationNeuron)network.Layers[0].Neurons[0]).Threshold;
             weights[1]= network.Layers[0].Neurons[0].Weights[0];
             weights[2]=network.Layers[0].Neurons[0].Weights[1];
@@ -99,13 +99,15 @@ namespace Perceptron
         [STAThread]
         static void Main()
         {
-            network = new ActivationNetwork(new SigmoidFunction(), 2, 1);
-            ((ActivationNeuron)network.Layers[0].Neurons[0]).Threshold = 0.5;
-            network.Layers[0].Neurons[0].Weights[0] = 0.3;
-            network.Layers[0].Neurons[0].Weights[1] = 0.2;
+            weights = new double[3];
+            network = new ActivationNetwork(new ThresholdFunction(), 2, 1);
+            weights[0]=((ActivationNeuron)network.Layers[0].Neurons[0]).Threshold = 0;
+            weights[1] = network.Layers[0].Neurons[0].Weights[0] = 0.9;
+            weights[2] = network.Layers[0].Neurons[0].Weights[1] = 0.2;
        
 
             learning = new PerceptronLearning(network);
+            learning.LearningRate = 0.05;
             var button=new Button { Top=0, Left=0 };
             button.Click+=NextSample;
             
@@ -127,7 +129,6 @@ namespace Perceptron
             public MyForm()
             {
                 DoubleBuffered = true;
-
             }
         }
     }
