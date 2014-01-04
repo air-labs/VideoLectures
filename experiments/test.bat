428fd8201d39f7cb6eb77e6ba776c19c725b7ef0
Rostislav Kirillov <superrast@gmail.com>
2014-01-05 00:17:58 +0300

    Fixed, now working on clean system

--- experiments/test.bat
+++ experiments/test.bat
@@ -1,8 +1,11 @@
 PATH C:\ffmpeg\bin\
-del *.mp4
+del fade*.avi
+del cross_fade.avi
+del intro.avi
+del watermark.avi
 
-ffmpeg -i fade_in.avs		fade_in.mp4 -vb 100 
-ffmpeg -i fade_out.avs		fade_out.mp4 -vb 100 
-ffmpeg -i cross_fade.avs	cross_fade.mp4 -vb 100 
-ffmpeg -i intro.avs			intro.mp4 -vb 100 
-ffmpeg -i watermark.avs		watermark.mp4 -vb 100 
+ffmpeg -i fade_in.avs -acodec copy -vcodec mpeg4 -r 30 -b:v 13M fade_in.avi
+ffmpeg -i fade_out.avs -acodec copy -vcodec mpeg4 -r 30 -b:v 13M fade_out.avi
+ffmpeg -i cross_fade.avs -acodec copy -vcodec mpeg4 -r 30 -b:v 13M cross_fade.avi
+ffmpeg -i intro.avs -acodec copy -vcodec mpeg4 -r 30 -b:v 13M intro.avi
+ffmpeg -i watermark.avs -acodec copy -vcodec mpeg4 -r 30 -b:v 13M watermark.avi
