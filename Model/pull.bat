766fe412fad87ded42f032ea42e0bcfc5b83c1de
okulovsky <yuri.okulovsky@gmail.com>
2014-01-31 17:52:49 +0600

    Все текстовые файлы

---
+++ Model/pull.bat
@@ -0,0 +1,6 @@
+copy ..\Montage\Editor\bin\Debug\Editor.exe .\
+copy ..\Montage\Montager\bin\Debug\Montager.exe .\
+copy ..\Montage\Assembler\bin\Debug\Assembler.exe .\
+copy ..\Montage\VideoLib\bin\Debug\VideoLib.dll .\
+copy ..\experiments\library.avs .\
+copy ..\experiments\image.png .\
