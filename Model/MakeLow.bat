77457343641d9625f51b6f5e57ba313c3878198c
okulovsky <yuri.okulovsky@gmail.com>
2014-02-01 23:25:46 +0600

    Добавил Renamer для итогового переименования
    Улучшил pull.bat для работы в других местах

--- Model/MakeLow.bat
+++
@@ -1,8 +0,0 @@
-Montager %1
-cd %1
-call MakeChunksLow
-cd ..
-call Clear %1
-Assembler %1
-cd %1
-call AssemblyHigh
--- Model/pull.bat
+++ Model/pull.bat
@@ -1,6 +1,9 @@
-copy ..\Montage\Editor\bin\Debug\Editor.exe .\
-copy ..\Montage\Montager\bin\Debug\Montager.exe .\
-copy ..\Montage\Assembler\bin\Debug\Assembler.exe .\
-copy ..\Montage\ImageGenerator\bin\Debug\ImageGenerator.exe .\
-copy ..\Montage\VideoLib\bin\Debug\VideoLib.dll .\
-copy ..\experiments\library.avs .\
+SET F=..\Montage
+
+copy %F%\Editor\bin\Debug\Editor.exe .\
+copy %F%\Montager\bin\Debug\Montager.exe .\
+copy %F%\Assembler\bin\Debug\Assembler.exe .\
+copy %F%\ImageGenerator\bin\Debug\ImageGenerator.exe .\
+copy %F%\Renamer\bin\Debug\Renamer.exe .\
+copy %F%\VideoLib\bin\Debug\VideoLib.dll .\
+copy %F%\library.avs .\
