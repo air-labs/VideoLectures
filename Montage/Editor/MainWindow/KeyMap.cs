aefddad74025d19112bce2e9b61d319fdf0d0abb
okulovsky <yuri.okulovsky@gmail.com>
2014-02-25 10:33:32 +0600

    Добавлен прототип KeyMap

---
+++ Montage/Editor/MainWindow/KeyMap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Editor
+{
+    public enum EditorKeys
+    {
+        Unknown,
+
+        Pause,
+        Forward,
+        Backward,
+        SpeedUp,
+        SpeedDown,
+
+        GeneralScreen,
+        GeneralFace,
+        GeneralDrop,
+        GeneralReset,
+        GeneralNextChunk,
+        GeneralPreviousChunk,
+        GeneralLeftBorderToLeft,
+        GeneralLeftBorderToRight,
+        GeneralRightBorderToLeft,
+        GeneralRightBorderToRight,
+
+
+        BorderLeftForderToLeft,
+        BorderLeftBorderToRight,
+        BorderRightBorderToLeft,
+        BorderRightBorderToRight
+    }
+
+    public static class KeyMap
+    {
+        Dictionary<Key, EditorKeys> map = new Dictionary<Key, EditorKeys>
+        {
+        };
+
+        public static EditorKeys GetKey(Key key)
+        {
+            return EditorKeys.Unknown;
+        }
+
+    }
+}
