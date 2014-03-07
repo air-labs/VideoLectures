2242f359d632e1d400cc20ca1da3adb0f38d0113
okulovsky <yuri.okulovsky@gmail.com>
2014-03-07 15:31:33 +0600

    Убрал модель и ее чтение в отдельный проект (где будет в будущем вся прилага). Выделил чтение модели в отдельный класс для упрощения версионности

---
+++ Montage/NewName/Model/ModelIO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Editor
+{
+    public class ModelIO
+    {
+
+        static EditorModel ParseV1(FileInfo file)
+        {
+            return new JavaScriptSerializer().Deserialize<EditorModel>(File.ReadAllText(file.FullName));
+        }
+
+
+        public static EditorModel Load(DirectoryInfo rootFolder, DirectoryInfo videoFolder)
+        {
+            if (!rootFolder.Exists)
+                throw new Exception("Root directory " + rootFolder.FullName + " is not found");
+            if (!videoFolder.Exists)
+                throw new Exception("Video directory " + rootFolder.FullName + " is not found");
+
+            var fileV1 = videoFolder.GetFiles("montage.editor");
+            if (fileV1.Length == 1)
+                return ParseV1(fileV1[0]);
+
+            var model = new EditorModel
+            {
+                Shift = 0,
+                TotalLength = 90 * 60 * 1000 //TODO: как-то по-разумному определить это время
+            };
+            model.Chunks.Add(new ChunkData { StartTime = 0, Length = model.TotalLength, Mode = Mode.Undefined });
+            return model;
+        }
+    }
+}
---
+++ Montage/NewName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
