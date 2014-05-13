13770aac102dd4aff5856458dbeca5ea3acfb7ba
Rostislav Kirillov <superrast@gmail.com>
2014-05-14 04:17:29 +0600

    fixed pull.bat

--- Model/pull.bat
+++ Model/pull.bat
@@ -9,4 +9,5 @@ copy %F%\VideoLib\bin\Debug\VideoLib.dll .\
 copy %F%\library.avs .\
 copy %F%\Splitter\bin\Debug\Splitter.exe .\
 copy %F%\split_pauses.praat .\
-copy %F%\praatcon.exe
+copy %F%\praatcon.exe .\
+copy %F%\autolevels_0.6_20110109.dll .\
